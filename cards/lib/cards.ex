f80b5ec8c495ff96b51c701e91b044e56e211cac
acatejr <acatejr@gmail.com>
2024-07-11 16:32:26 -0700

    Worked on pattern matching

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,27 +3,47 @@ defmodule Cards do
   Documentation for `Cards`.
 
   Everything in elixir is in modules, like this one.  A module is a collection of methods.
+
+  Pattern matching is core to elixir.
+  Pattern matching is elixir's replacement for variable assignment.
+  Pattern matching is used anytime = is used.
+  {} are tuples in elixir
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  # def hello do
-  #   :world
-  # end
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+
+    # This does the same thing as the comment block above.
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
   end
 
   def shuffle(deck) do
+    # Immutability - copies input list, shuffles the copy and returns the copy
+    # The input is never changed.
+
     Enum.shuffle(deck)
   end
 
+  def contains?(deck, card) do
+    # Elixir can have question mark in method name.
+    # Convention makes this boolean return value
+
+    Enum.member?(deck, card)
+  end
 end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,22 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  # test "greets the world" do
+  #   assert Cards.hello() == :world
+  # end
+
+  test "card is in deck" do
+    deck = Cards.create_deck
+    card = "Ace of Spades"
+
+    assert Cards.contains?(deck, card) == true
   end
+
+  test "card is not in deck" do
+    deck = Cards.create_deck
+    card = "Ace of Spikes"
+
+    assert Cards.contains?(deck, card) == false
+  end
+
 end
